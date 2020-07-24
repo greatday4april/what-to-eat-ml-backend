59c4bcc9767308761797665e7280611272487aa0
greatday4april <greatday4april@gmail.com>
2020-07-24 14:57:22 -0700

    set up django
    Co-authored-by: fish518518@hotmail.com <fish518518@hotmail.com>

---
+++ what_to_eat/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for what_to_eat project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'what_to_eat.settings')
+
+application = get_wsgi_application()
