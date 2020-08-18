c89051e5cbe5142cbc1537b5df7df5cb51e75161
April Li <greatday4april@gmail.com>
2020-08-17 18:23:14 -0700

    add support for deployment

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ CACHES = {
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
 SESSION_SAVE_EVERY_REQUEST = True
+
+django_heroku.settings(locals())
